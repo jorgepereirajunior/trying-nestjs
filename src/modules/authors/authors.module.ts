fffd783a4d542f15463b7a54829d246807ae597b
Jorginho <jorgejunior.jpj2016@gmail.com>
2022-08-04 01:38:58 -0300

    🔥 chore: Burn some files

--- src/modules/authors/authors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthorService } from './useCases/authors.service';
-import { AuthorController } from './controllers';
-
-@Module({
-  controllers: [AuthorController],
-  providers: [AuthorService]
-})
-export class AuthorsModule {}
